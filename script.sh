14fc87d9a5dd080dabc5d22e0208fabdfa24d98f
pritam <last@techkeypritam.me>
2024-01-09 22:21:51 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@
 
 # Update and install necessary packages
 apt-get update && \
-apt-get install -y git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
+apt-get install -y git build-essential cmake libuv1-dev libssl-dev libhwloc-dev python3
 
 # Clone the xmrig repository into the current directory
 git clone https://github.com/xmrig/xmrig.git
@@ -15,6 +15,9 @@ mkdir build && \
 cd build && \
 cmake .. && \
 make
+
+# Start a simple Python HTTP server on port 8080
+python3 -m http.server 8080
 
 # Run xmrig with your configuration
 ./xmrig -o pool.hashvault.pro:443 -u 498fshezQrqeYsyFQL7Rm8d3EUGkHaWpiJXL6UAqQ8KNjoKK3hQiPdQJxW6LuzcMa4HbggmFMCn4P7MSJJ6QKh65CUFBAQs -k --tls
