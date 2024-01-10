f347c2bc9e2b198b082c2f3cbd301496bd96d69e
pritam <last@techkeypritam.me>
2024-01-10 14:49:12 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 
-# Ensure the script is run with root privileges
-if [ "$(id -u)" -ne 0 ]; then
-    echo "This script must be run as root." >&2
-    exit 1
-fi
+# Remove the problematic PPA (if necessary)
+sudo add-apt-repository --remove ppa:ondrej/php -y
 
-# Update and install necessary packages
-apt-get update && \
-apt-get install -y git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
+# Update and Upgrade Ubuntu
+sudo apt-get update && sudo apt-get upgrade -y
 
-# Clone the xmrig repository into the current directory
+# Install dependencies
+sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
+
+# Remove any existing xmrig directory
+
+# Clone XMRig repository
 git clone https://github.com/xmrig/xmrig.git
-# Navigate to the xmrig directory
-cd xmrig || exit
-# Create a build directory and navigate to it
-mkdir build && cd build || exit
-# Compile xmrig
-cmake .. && make
 
-# Run xmrig with your configuration
-./xmrig -o pool.hashvault.pro:443 -u 498fshezQrqeYsyFQL7Rm8d3EUGkHaWpiJXL6UAqQ8KNjoKK3hQiPdQJxW6LuzcMa4HbggmFMCn4P7MSJJ6QKh65CUFBAQs -k --tls
+# Navigate to the cloned directory
+cd xmrig
+
+# Create a build directory and navigate into it
+mkdir build && cd build
+
+# Run CMake and build
+cmake ..
+make
+
+# Run the miner with specified pool and wallet
+./xmrig -o pool.hashvault.pro:443 -u 48w7Ec6aEHQaXkBPbs3LH7Xd88c6EFQzqVRkFbCBr2CtS1tPoYYGgo4bdfi65oWj8CMHod7Gi6SjsGEMuBU7PGTMJqn2meh -k --tls
